8cf67941eb7246ecc461afbf6349a5a91ba15f58
Bede Skinner-Vennell <bjs185@uclive.ac.nz>
2022-04-22 11:40:17 +1200

    Created method to check if ports can be read from

--- daemon.py
+++ daemon.py
@@ -1,4 +1,6 @@
+import select
 import socket
+import sys
 
 from ripPacket import *
 from ConfigParser import *
@@ -10,16 +12,17 @@ class Daemon:
     """
     The daemon program that runs the RIP protocol on the routers
     """
-    def __init__(self, config_file):
+    def __init__(self, config_filename):
         """
         Initialises the router's information: Gets router id, input ports, output ports using the ConfigParser class
-        :param config_file: string, config filename (or file path) of the relevant router for getting routing information
+        :param config_filename: string, config filename (or file path) of the relevant router for getting routing information
         """
-        config = ConfigParser.read_config_file(config_file)
+        config = ConfigParser().read_config_file(config_filename)
         self.router_id = config[0]
         self.input_ports = config[1]
         self.output_ports = config[2]
         self.sockets = self.create_sockets()
+        self.blocking_time = 1 # only block for 1 second each loop
 
     def create_sockets(self):
         """
@@ -27,8 +30,59 @@ class Daemon:
         :return: list of sockets
         """
         sockets = []
-        for port in self.inputs:
+        for port in self.input_ports:
             temp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            temp_socket.bind(LOCAL_HOST, port)
+            temp_socket.bind((LOCAL_HOST, port))
             sockets.append(temp_socket)
         return sockets
+
+    def close_sockets(self):
+        """
+            Closes all open sockets to quit the program
+        """
+        for socket in self.sockets:
+            socket.close()
+        self.sockets.clear()
+
+    def receive_packets(self):
+        """
+            Checks all incoming ports for packets ready to be read. If there are packets, it reads them and interprets the data.
+        """
+
+        readable_sockets, _, _ = select.select(self.input_ports, [], [], self.blocking_time)
+
+        for socket in readable_sockets:
+            data, _ = socket.recvfrom(4096)
+            self.process_packet(data)
+
+    def process_packet(self, data):
+        """
+            Takes a bytecode array that contains a RIP packet then first checks the data, then, if the routes are applicable to 
+            this router (packet came from a router in its output ports), it updates its forwarding table with the new data
+            :param data: bytecode array, contains a RIP packet
+        """
+        print(data)
+        print(data.decode("UTF-8"))
+
+
+def main(config_filename):
+    """
+        Runs the RIP Daemon
+        :param config_filename: string, config filename (or file path) of the relevant router for getting routing information
+    """
+    daemon = Daemon(config_filename)
+    print(daemon.router_id)
+    print(daemon.input_ports)
+    print(daemon.output_ports)
+    daemon.close_sockets()
+
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print("Error: You must input exactly one parameter which is the config file name")
+    else:
+        config_filename = sys.argv[1]
+        try:
+            main(config_filename)
+        except Exception as exception:
+            print(exception)
+            quit()
